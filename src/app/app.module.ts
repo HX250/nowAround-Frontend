188a039b28755e5d44fef33926bb1ef553a0e19a
Rastislav Pacut <rastislavpacut250@gmail.com>
2024-06-27 12:46:16 +0200

    hero section on landing page done with photos and text

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule, routingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, routingComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/Utils/landing-page/landing-page.component.ts
+++ src/app/components/Utils/landing-page/landing-page.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.css']
+  styleUrls: ['./landing-page.component.css'],
+  animations: [
+    trigger('slideAnimation', [
+      state('previous', style({ transform: 'translateX(-100%)' })),
+      state('current', style({ transform: 'translateX(0)' })),
+      state('next', style({ transform: 'translateX(100%)' })),
+      transition('previous => current, current => next', [
+        animate('0.8s ease-out'),
+      ]),
+      transition('next => current, current => previous', [
+        animate('0.8s ease-out'),
+      ]),
+    ]),
+  ],
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit {
+  currentIndex: number = 0;
 
+  images: string[] = [
+    'url(/assets/landing-page/landingPage-city.jpg)',
+    'url(/assets/landing-page/landingPage-nature.jpg)',
+    'url(/assets/landing-page/landingPage-coffee.jpg)',
+  ];
+
+  ngOnInit(): void {
+    this.startCarousel();
+  }
+
+  startCarousel(): void {
+    setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.images.length;
+    }, 5000);
+  }
+
+  getState(index: number): string {
+    if (index === this.currentIndex) {
+      return 'current';
+    } else if (
+      index ===
+      (this.currentIndex - 1 + this.images.length) % this.images.length
+    ) {
+      return 'previous';
+    } else if (index === (this.currentIndex + 1) % this.images.length) {
+      return 'next';
+    } else {
+      return '';
+    }
+  }
 }
