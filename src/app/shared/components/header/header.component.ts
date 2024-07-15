26d0d76be9d544c090df41f805d59df2adc94d7b
Rastislav Pacut <rastislavpacut250@gmail.com>
2024-07-15 21:32:39 +0200

    changed colors, added navbar to header, also implemented language select box

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,8 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  languageSet: string = '0';
+
+  ngOnInit(): void {
+    const storedLang = localStorage.getItem('lang');
+    if (storedLang) {
+      this.languageSet = storedLang;
+    } else {
+      localStorage.setItem('lang', this.languageSet);
+    }
+  }
+
+  changeLang(value: string) {
+    localStorage.setItem('lang', value);
+    this.languageSet = value;
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [FooterComponent, HeaderComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   exports: [FooterComponent, HeaderComponent],
 })
 export class SharedModule {}
