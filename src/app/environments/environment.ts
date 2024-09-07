075f9ade61c8b97f9a45a8b1bbc78d58c2667e47
Rastislav Pacut <rastislavpacut250@gmail.com>
2024-09-07 22:13:57 +0200

    removed env files from commit

--- src/app/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  mapbox: {
-    accessToken:
-      'pk.eyJ1IjoiaHgyNTAiLCJhIjoiY20wc2t3dWRqMGFsZjJpcjYyZ212Z2s4aSJ9.hIENNnrmndstvOS9HQyhBw',
-  },
-};
