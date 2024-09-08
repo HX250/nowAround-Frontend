1f42e1153fbc4a991ba3e32c37502e3f9c9d8291
Rastislav Pacut <rastislavpacut250@gmail.com>
2024-09-08 11:22:39 +0200

    env files

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   mapbox: {
-    accessToken: 'REPLACE_WITH_MAPBOX_TOKEN',
+    accessToken:
+      'pk.eyJ1IjoiaHgyNTAiLCJhIjoiY20wc2wzeWx4MGx4NDJqc2h3MnZpaHVoNCJ9.lc793x-33PqNLDzBMBnPkw',
   },
 };
--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
   mapbox: {
-    accessToken: '',
+    accessToken:
+      'pk.eyJ1IjoiaHgyNTAiLCJhIjoiY20wc2wzeWx4MGx4NDJqc2h3MnZpaHVoNCJ9.lc793x-33PqNLDzBMBnPkw',
   },
 };
