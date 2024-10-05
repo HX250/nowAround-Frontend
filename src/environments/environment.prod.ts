9baa640f2c726a3e06188ffe31f1b8d62fa299db
Rastislav Pacut <rastislavpacut250@gmail.com>
2024-10-05 11:07:15 +0200

    updated env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,5 @@
 export const environment = {
   production: true,
-  apiEndpoint: process.env['API_END_POINT'],
-  mapbox: {
-    accessToken: process.env['MAPBOX_TOKEN'],
-  },
+  API_END_POINT: '',
+  MAPBOX_TOKEN: '',
 };
