a898a644dfaa1be681d0842e605509090b47ed84
Rastislav Pacut <rastislavpacut250@gmail.com>
2024-10-20 10:28:30 +0200

    popUp window for registering establishment

--- src/app/components/estabilishmentFeature/establishment-popup/establishment-popup.component.ts
+++ src/app/components/estabilishmentFeature/establishment-popup/establishment-popup.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-establishment-popup',
   templateUrl: './establishment-popup.component.html',
-  styleUrls: ['./establishment-popup.component.css']
+  styleUrls: ['./establishment-popup.component.css'],
 })
 export class EstablishmentPopupComponent {
-
+  @Input() establishmentEmail: string | undefined;
 }
--- src/app/components/estabilishmentFeature/establishment-register-form/establishment-register-form.component.ts
+++ src/app/components/estabilishmentFeature/establishment-register-form/establishment-register-form.component.ts
@@ -2,6 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 import { EstablishmentRegisterComponent } from '../establishment-register/establishment-register.component';
 import { EstablishmentRegisterPersonalInfoComponent } from '../establishment-register-personal-info/establishment-register-personal-info.component';
 import { EstabilishmentService } from 'src/app/services/estabilishmentService/estabilishment.service';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-establishment-register-form',
@@ -11,6 +12,7 @@ import { EstabilishmentService } from 'src/app/services/estabilishmentService/es
 export class EstablishmentRegisterFormComponent {
   error: boolean = false;
   popUpHidden: boolean = true;
+  estEmail: string = '';
 
   @ViewChild(EstablishmentRegisterComponent)
   establishmentRegisterComponent!: EstablishmentRegisterComponent;
@@ -27,18 +29,22 @@ export class EstablishmentRegisterFormComponent {
 
     if (personalValid && establishmentValid) {
       const completeFormData = { personalInfo, establishmentInfo };
-      this.estService.registerEstablishment(completeFormData).subscribe({
-        next: (response) => {
-          console.log('Success:', response);
-        },
-        error: (error) => {
-          console.error('Error:', error);
-        },
-        complete: () => {
-          this.popUpHidden = false;
-          console.log('Request completed.');
-        },
-      });
+      this.estService
+        .registerEstablishment(completeFormData)
+        .pipe(take(1))
+        .subscribe({
+          next: (response) => {
+            console.log('Success:', response);
+          },
+          error: (error) => {
+            console.error('Error:', error);
+          },
+          complete: () => {
+            this.estEmail = completeFormData.personalInfo.email;
+            this.popUpHidden = false;
+            console.log('Request completed.');
+          },
+        });
     } else {
       this.error = true;
     }
