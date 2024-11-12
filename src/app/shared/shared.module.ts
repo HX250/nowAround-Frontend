7f4732c5ebc88b82f6215860f9bc31f6f658eb17
Rastislav Pacut <rastislavpacut250@gmail.com>
2024-11-12 10:28:34 +0100

    migrating to angular 18

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FormsModule } from '@angular/forms';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
@@ -15,27 +15,21 @@ export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 
-@NgModule({
-  declarations: [
-    FooterComponent,
-    HeaderComponent,
-    NotFoundPageComponent,
-    ScrollArrowUpComponent,
-    PopupWindowComponent,
-  ],
-  imports: [
-    CommonModule,
-    FormsModule,
-    HttpClientModule,
-    TranslateModule,
-    RouterModule,
-  ],
-  exports: [
-    FooterComponent,
-    HeaderComponent,
-    NotFoundPageComponent,
-    ScrollArrowUpComponent,
-    PopupWindowComponent,
-  ],
-})
+@NgModule({ declarations: [
+        FooterComponent,
+        HeaderComponent,
+        NotFoundPageComponent,
+        ScrollArrowUpComponent,
+        PopupWindowComponent,
+    ],
+    exports: [
+        FooterComponent,
+        HeaderComponent,
+        NotFoundPageComponent,
+        ScrollArrowUpComponent,
+        PopupWindowComponent,
+    ], imports: [CommonModule,
+        FormsModule,
+        TranslateModule,
+        RouterModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class SharedModule {}
