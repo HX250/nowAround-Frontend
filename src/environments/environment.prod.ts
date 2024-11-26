431970e77adad40104e7c3dadcf58bf672a4641e
Rastislav Pacut <rastislavpacut250@gmail.com>
2024-11-26 11:29:04 +0100

    env changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
 export const environment = {
   API_END_POINT: '',
-  MAPBOX_TOKEN:
-    'pk.eyJ1IjoiaHgyNTAiLCJhIjoiY20yaXhmaXFoMHAxeTJqc2JsZnQ1eXZpOCJ9.mhFWHebJ4TRzyp3Tj2bTYw',
+  MAPBOX_TOKEN: '',
 };
-//https://app-nowaround-prod.azurewebsites.net/api/
