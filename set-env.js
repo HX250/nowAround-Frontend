b96da42a39cb9cbcb0a43ef2153adeba85dcd517
Rastislav Pacut <rastislavpacut250@gmail.com>
2024-12-14 14:27:53 +0100

    script

--- set-env.js
+++ set-env.js
@@ -8,13 +8,10 @@ const envFilePath = path.join(
 
 let fileContent = fs.readFileSync(envFilePath, "utf8");
 
-if (process.env.MAPBOX_TOKEN && process.env.API_END_POINT) {
-  fileContent = fileContent
-    .replace("process.env.MAPBOX_TOKEN", `"${process.env.MAPBOX_TOKEN}"`)
-    .replace("process.env.API_END_POINT", `"${process.env.API_END_POINT}"`);
+fileContent = fileContent
+  .replace("process.env.MAPBOX_TOKEN", `"${process.env.MAPBOX_TOKEN}"`)
+  .replace("process.env.API_END_POINT", `"${process.env.API_END_POINT}"`);
 
-  fs.writeFileSync(envFilePath, fileContent, "utf8");
-  console.log("Environment variables injected into environment.prod.ts");
-} else {
-  console.error("MAPBOX_TOKEN or API_END_POINT are missing.");
-}
+fs.writeFileSync(envFilePath, fileContent, "utf8");
+
+console.log("Environment variables injected into environment.prod.ts");
