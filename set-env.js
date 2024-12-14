e1360f010f6bbd41fdf9517abd73cf237ef6f5fb
Rastislav Pacut <rastislavpacut250@gmail.com>
2024-12-14 13:33:58 +0100

    script changes

--- set-env.js
+++ set-env.js
@@ -2,23 +2,13 @@ const fs = require("fs");
 const path = require("path");
 const successColor = "\x1b[32m%s\x1b[0m";
 const checkSign = "\u{2705}";
-
-const apiEndpoint = process.env.API_END_POINT;
-const mapboxToken = process.env.MAPBOX_TOKEN;
-
-if (!apiEndpoint || !mapboxToken) {
-  console.error(
-    "Environment variables API_END_POINT or MAPBOX_TOKEN are missing!",
-  );
-  process.exit(1);
-}
+const dotenv = require("dotenv").config({ path: "src/.env" });
 
 const envFile = `export const environment = {
-    API_END_POINT: '${apiEndpoint}',
-    MAPBOX_TOKEN: '${mapboxToken}',
+    API_END_POINT: '${process.env.API_END_POINT}',
+    MAPBOX_TOKEN: '${process.env.MAPBOX_TOKEN}',
 };
 `;
-
 const targetPath = path.join(
   __dirname,
   "./src/environments/environment.prod.ts",
@@ -27,9 +17,10 @@ fs.writeFile(targetPath, envFile, (err) => {
   if (err) {
     console.error(err);
     throw err;
+  } else {
+    console.log(
+      successColor,
+      `${checkSign} Successfully generated environment.prod.ts`,
+    );
   }
-  console.log(
-    successColor,
-    `${checkSign} Successfully generated environment.prod.ts`,
-  );
 });
