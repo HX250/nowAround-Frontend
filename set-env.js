ea2c93a0afd180ac6de6637d128f8a8ccb596d49
Rastislav Pacut <rastislavpacut250@gmail.com>
2024-12-14 14:32:51 +0100

    script

--- set-env.js
+++ set-env.js
@@ -1,17 +1,20 @@
 const fs = require("fs");
 const path = require("path");
 
-const envFilePath = path.join(
+const envFile = `export const environment = {
+    API_END_POINT: '${process.env.API_END_POINT}',
+    MAPBOX_TOKEN: '${process.env.MAPBOX_TOKEN}',
+};
+`;
+const targetPath = path.join(
   __dirname,
   "./src/environments/environment.prod.ts",
 );
-
-let fileContent = fs.readFileSync(envFilePath, "utf8");
-
-fileContent = fileContent
-  .replace("process.env.MAPBOX_TOKEN", `"${process.env.MAPBOX_TOKEN}"`)
-  .replace("process.env.API_END_POINT", `"${process.env.API_END_POINT}"`);
-
-fs.writeFileSync(envFilePath, fileContent, "utf8");
-
-console.log("Environment variables injected into environment.prod.ts");
+fs.writeFile(targetPath, envFile, (err) => {
+  if (err) {
+    console.error(err);
+    throw err;
+  } else {
+    console.log(` Successfully generated environment.prod.ts`);
+  }
+});
